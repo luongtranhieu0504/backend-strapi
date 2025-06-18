65e3d75b129b73c2d48dd068751cc922778b9544
luong <luongtranhieu2@gmail.com>
2025-06-18 10:37:45 +0700

    update override find populate to user and post

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -25,17 +25,44 @@ module.exports = createCoreController('api::post.post', ({ strapi }) => ({
     };
   },
   async find(ctx) {
+    // Đảm bảo populate author và liked_by
+    ctx.query = {
+      ...ctx.query,
+      populate: {
+        author: true,
+        liked_by: true,
+      },
+    };
+
     const { data, meta } = await super.find(ctx);
 
-    // Flatten each post
+    // Flatten each post, lấy thông tin user và liked_by
     const flatData = data.map(post => ({
       id: post.id,
       ...post.attributes,
+      author: post.attributes.author?.data
+        ? {
+            id: post.attributes.author.data.id,
+            username: post.attributes.author.data.attributes.username,
+            email: post.attributes.author.data.attributes.email,
+            name: post.attributes.author.data.attributes.name,
+            photoUrl: post.attributes.author.data.attributes.photoUrl,
+          }
+        : null,
+      liked_by: post.attributes.liked_by?.data
+        ? post.attributes.liked_by.data.map(user => ({
+            id: user.id,
+            username: user.attributes.username,
+            name: user.attributes.name,
+            photoUrl: user.attributes.photoUrl,
+          }))
+        : [],
     }));
 
     return {
       status: 'success',
       data: flatData,
+      meta,
       message: 'OK',
     };
   },
