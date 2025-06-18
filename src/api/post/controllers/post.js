8d4ceb5f74b931666d000d6d31ae33b9a97d45c1
luong <luongtranhieu2@gmail.com>
2025-06-18 10:24:36 +0700

    update post controller to handle new validation logic

--- src/api/post/controllers/post.js
+++ src/api/post/controllers/post.js
@@ -6,4 +6,37 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::post.post');
+module.exports = createCoreController('api::post.post', ({ strapi }) => ({
+  async create(ctx) {
+    // Gọi hàm create gốc
+    const response = await super.create(ctx);
+
+    // Lấy data gốc
+    const { id, attributes } = response.data;
+
+    // Trả về dữ liệu phẳng
+    return {
+      status: 'success',
+      data: {
+        id,
+        ...attributes,
+      },
+      message: 'OK',
+    };
+  },
+  async find(ctx) {
+    const { data, meta } = await super.find(ctx);
+
+    // Flatten each post
+    const flatData = data.map(post => ({
+      id: post.id,
+      ...post.attributes,
+    }));
+
+    return {
+      status: 'success',
+      data: flatData,
+      message: 'OK',
+    };
+  },
+}));
